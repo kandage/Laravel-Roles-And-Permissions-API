25b2de6c4f4148adfaabd055841ad91311efdc80
kandage <kandagehiruni@gmail.com>
2023-04-13 21:59:21 +0530

    first commit

---
+++ homeCare/app/Http/Controllers/Api/LoginController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Http\Helpers\Helper;
+use App\Http\Resources\UserResource;
+use Auth;
+
+class LoginController extends Controller
+{
+    public function login(LoginRequest $request)
+    {
+        if(!Auth::attempt($request->only('email','password'))){
+            Helper::sendError('Email Or Password is wroing !!!');
+        }
+
+        return new UserResource(auth()->user());
+    }
+}
---
+++ homeCare/app/Http/Controllers/Api/RegisterController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\RegisterRequest;
+use App\Http\Helpers\Helper;
+use App\Http\Resources\UserResource;
+use Spatie\Permission\Models\Role;
+use App\Models\User;
+
+
+class RegisterController extends Controller
+{
+    public function register(RegisterRequest $request)
+    {
+        $user = User::create([
+            'name'          => $request->name,
+            'email'         => $request->email,
+            'password'      => bcrypt($request->password)
+        ]);
+
+        $user_role = Role::where(['name' => 'user'])->first();
+        if($user_role){
+            $user->assignRole($user_role);
+        }
+        return new UserResource($user);
+    }
+}
